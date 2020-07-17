80a712626e1e3bf96b4c941e92c700b0e0a60020
Rob Bassi <rob.bassi.89@gmail.com>
2020-07-17 19:03:39 -0400

    More refactoring

--- commitlog.py
+++ commitlog.py
@@ -2,30 +2,27 @@ from os import path
 from memtable import Memtable
 
 class CommitLog:
-    def __init__(self, log_path):
-        self.fd = None
-        self.log_path = log_path
-        self.recovered_memtable = None
-        self.__resume()
-     
-    def __resume(self):
+    def __init__(self, fd):
+        self.fd = fd
+
+    @classmethod 
+    def resume(self, log_path):
+        memtable = Memtable()
+        fd = None
         # recover the memtable if necessary
-        if path.exists(self.log_path):
-            memtable = Memtable()
-            self.fd = open(self.log_path, 'r+')
-            record = self.fd.readline()
+        if path.exists(log_path):
+            fd = open(log_path, 'r+')
+            record = fd.readline()
             while record:
                 key, value = record.rstrip().split(',')
                 if value:
                     memtable.set(key, value)
                 else:
                     memtable.unset(key)
-                record = self.fd.readline()
-            # check if it has any data
-            if memtable.approximate_bytes() > 0:
-                self.recovered_memtable = memtable
+                record = fd.readline()
         else:
-            self.fd = open(self.log_path, 'w')
+            fd = open(log_path, 'w')
+        return CommitLog(fd), memtable
 
     def record_set(self, k, v):
         self.fd.write(f"{k},{v}\n")
@@ -36,6 +33,5 @@ class CommitLog:
         self.fd.flush()
 
     def purge(self):
-        self.fd.close()
-        self.fd = open(self.log_path, 'w')
-
+        self.fd.seek(0)
+        self.fd.truncate()
