7cc0d59252e158c7e7abe116d7f02d7311045c57
John Gerassimou <john.gerassimou@lcbo.com>
2020-07-21 09:55:37 -0400

    Remove contextmanager and move fd open to each method

--- sstable.py
+++ sstable.py
@@ -1,17 +1,18 @@
-from contextlib import contextmanager
 from os import path
 
 from memtable import TOMBSTONE
 
 
 class SSTable:
+    """Represents a Sorted-String-Table (SSTable) on disk"""
 
     def __init__(self, sstable_path):
         self.path = sstable_path
         self.fd = None
 
     def write(self, memtable):
-        with self.open('w'):
+        self.fd = open(self.path, 'w')
+        with open(self.path, 'w'):
             for (key, value) in memtable.entries():
                 if value == TOMBSTONE:
                     entry = f"{key},\n"
@@ -21,17 +22,10 @@ class SSTable:
 
     def search(self, search_key):
         if path.exists(self.path):
-            with self.open('r') as sstable:
-                for line in sstable.fd:
+            self.fd = open(self.path, 'r')
+            with open(self.path, 'r') as sstable:
+                for line in sstable:
                     (key, value) = line.rstrip().split(',')
                     if key == search_key:
                         if value:
                             return value
-
-    @contextmanager
-    def open(self, mode):
-        try:
-            self.fd = open(self.path, mode)
-            yield self
-        finally:
-            self.fd.close()
