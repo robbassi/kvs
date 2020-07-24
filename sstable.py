b48bb5289e9bc12efc6abadc2a7ae1bc15a90cf9
John Gerassimou <john.gerassimou@lcbo.com>
2020-07-24 05:04:17 -0400

    Modify sstable class to support segments requirement

--- sstable.py
+++ sstable.py
@@ -1,31 +1,45 @@
-from os import path
-
+from bloomfilter import BloomFilter
 from memtable import TOMBSTONE
+
+BF_SIZE = 10000
+BF_HASH_COUNT = 5
 
 
 class SSTable:
     """Represents a Sorted-String-Table (SSTable) on disk"""
 
-    def __init__(self, sstable_path):
-        self.path = sstable_path
-        self.fd = None
+    def __init__(self, fd, bf=None):
+        self.fd = fd
+        self.bf = bf
+        if not self.bf:
+            self.__sync()
 
-    def write(self, memtable):
-        self.fd = open(self.path, 'w')
-        with open(self.path, 'w'):
+    def __sync(self):
+        self.bf = BloomFilter(BF_SIZE, BF_HASH_COUNT)
+        for (key, value) in self.entries():
+            self.bf.add(key)
+
+    @classmethod
+    def create(cls, path, memtable):
+        bf = BloomFilter(BF_SIZE, BF_HASH_COUNT)
+        fd = open(path, 'w')
+        with fd:
             for (key, value) in memtable.entries():
                 if value == TOMBSTONE:
-                    entry = f"{key},\n"
+                    fd.write(f"{key},\n")
                 else:
-                    entry = f"{key},{value}\n"
-                self.fd.write(entry)
+                    fd.write(f"{key},{value}\n")
+                bf.add(key)
+        return cls(fd, bf)
+
+    def entries(self):
+        yield from (line.rstrip().split(',') for line in open(self.fd.name, 'r'))
 
     def search(self, search_key):
-        if path.exists(self.path):
-            self.fd = open(self.path, 'r')
-            with open(self.path, 'r') as sstable:
-                for line in sstable:
-                    (key, value) = line.rstrip().split(',')
-                    if key == search_key:
-                        if value:
-                            return value
+        if not self.bf.exists(search_key):
+            return None
+        for (key, value) in self.entries():
+            if key == search_key:
+                if value:
+                    return value
+        return None
