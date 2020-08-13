993a25f91c8e136341c83ac8d5be86351688383b
Anar Manafov <anarmanafov1@gmail.com>
2020-08-13 12:15:58 -0400

    responded to pr feedback

--- kvs.py
+++ kvs.py
@@ -10,29 +10,23 @@ MT_MAX_SIZE = 5000000
 class KVS:
     def __init__(self, segments_path, log_path):
         self.rwlock = rwlock.RWLockFairD()
-        self.commitlog, self.memtable = CommitLog(log_path).resume(log_path)
+        self.commitlog, self.memtable = CommitLog.resume(log_path)
         self.segments = Segments(segments_path)
 
     def get(self, k):
         with self.rwlock.gen_rlock():
-            r = self.memtable.get(k)
-            if r is None:
-                return self.segments.search(k)
-            else:
-                return r
+            return v if (v := self.memtable.get(k)) else self.segments.search(k)
 
     def set(self, k, v):
         with self.rwlock.gen_wlock():
-            if self.memtable.approximate_bytes() <= MT_MAX_SIZE:
-                self.commitlog.record_set(k, v)
-                self.memtable.set(k, v)
-            else:
-                self.commitlog.record_set(k, v)
-                self.memtable.set(k, v)
+            self.commitlog.record_set(k, v)
+            self.memtable.set(k, v)
+            if self.memtable.approximate_bytes() >= MT_MAX_SIZE:
                 self.segments.flush(self.memtable)
                 self.memtable = Memtable()
+                self.commitlog.purge()
 
-    def unset(self, k):
+def unset(self, k):
         with self.rwlock.gen_wlock():
             self.commitlog.record_unset(k)
             self.memtable.unset(k)
