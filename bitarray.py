bd97ccf1a9a18c246da0ea760bc37738b7cacd29
Farid Manafov <faridbardium@gmail.com>
2020-08-15 17:12:29 -0400

    made more explicit exceptions, and created a more byte sized arr per Robs suggestion

--- bitarray.py
+++ bitarray.py
@@ -1,39 +1,31 @@
+import array
+import math
+
 class bitarray:
-
     def __init__(self, size):
-        self.arr = [0] * size
+        self.arr = array.array('B', [0] * math.ceil(size/8))
         
-
     def setall(self, bit):
-
         if bit == 0 or bit == 1:
-            for i in range(len(self.arr)):
-                self.arr[i] = bit
+            self.arr = [bit] * len(self.arr)
         else:
-            raise Exception("SetAll: Invalid bit entered, only 1 or 0 are valid inputs.")
-
+            raise ValueError("SetAll: Invalid bit entered, only 1 or 0 are valid inputs.")
 
     def set(self, n, bit):
-
         if bit == 0 or bit == 1:
-
             try:
                 self.arr[n] = bit
             except IndexError:
-                raise Exception("Set: Value entered is out of range of the array.")
+                raise IndexError("Set: Value entered is out of range of the array.")
             except TypeError:
-                raise Exception("Set: Enter a value that is an integer")
-
+                raise TypeError("Set: Enter a value that is an integer")
         else:
             raise Exception("Set: Invalid bit entered, only 1 or 0 are valid inputs.")
 
-
-
     def get(self, n):
-
         try:
             return self.arr[n]
         except IndexError:
-            raise Exception("Get: Value entered is out of range of the array.")
+            raise IndexError("Get: Value entered is out of range of the array.")
         except TypeError:
-            raise Exception("Get: Value entered is not an integer.")
+            raise TypeError("Get: Value entered is not an integer.")
