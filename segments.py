05bd2f8885e01ee579d7970c3e28d88258f5a383
Anar Manafov <anarmanafov1@gmail.com>
2020-09-20 14:57:13 -0400

    wip

--- segments.py
+++ segments.py
@@ -1,9 +1,14 @@
 from sstable import SSTable
 from os import scandir, mkdir, path, remove
 from re import compile as compile_re
+from bloomfilter import BloomFilter
+from binio import kv_iter
 
 SEGMENT_TEMPLATE = 'segment-%d.dat'
 SEGMENT_PATTERN = compile_re("segment-(?P<index>\d+)\.dat")
+
+BF_SIZE = 10000
+BF_HASH_COUNT = 5
 
 from compaction import compaction_pass, compute_buckets
 
@@ -49,3 +54,8 @@ class Segments:
             old_files, new_file = files
             for old_file in old_files:
                 remove(old_file.path)
+                # TODO: remove deleted segments from in memory segments as they are being deleted on disk
+                for k, _ in kv_iter(new_file.path):
+                    new_bf = BloomFilter(BF_SIZE, BF_HASH_COUNT)
+                    new_bf.add(k)
+            self.segments += [SSTable(new_file.path, new_bf)]
