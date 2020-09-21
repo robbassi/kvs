8c3249d5e672b28c658834902771abf2735ae82e
Rob Bassi <rob.bassi.89@gmail.com>
2020-09-21 19:15:58 -0400

    Add index to SSTable

--- sstable.py
+++ sstable.py
@@ -8,8 +8,9 @@ BF_HASH_COUNT = 5
 class SSTable:
     """Represents a Sorted-String-Table (SSTable) on disk"""
 
-    def __init__(self, path, bf=None):
+    def __init__(self, path, index, bf=None):
         self.path = path
+        self.index = index
         self.bf = bf
         if not self.bf:
             self._sync()
@@ -23,13 +24,13 @@ class SSTable:
                 r.skip_value()
 
     @classmethod
-    def create(cls, path, memtable):
+    def create(cls, path, index, memtable):
         bf = BloomFilter(BF_SIZE, BF_HASH_COUNT)
         with kv_writer(path) as writer:
             for key, value in memtable.entries():
                 writer.write_entry(key, value)
                 bf.add(key)
-        return cls(path, bf)
+        return cls(path, index, bf)
 
     def search(self, search_key):
         if not self.bf.exists(search_key):
