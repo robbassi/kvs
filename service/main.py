110d8c7c7e7aa0270739d900b12f73403ce35ef8
John Gerassimou <john.gerassimou@lcbo.com>
2020-12-14 19:16:37 -0500

    fix test

--- service/main.py
+++ service/main.py
@@ -1,13 +1,13 @@
 import os
 import tempfile
-from typing import Tuple, List, Union
+from typing import Tuple, List
 
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse, Response
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
-from common import TOMBSTONE
 from binio import kv_iter
+from common import TOMBSTONE
 from kvs import KVS
 
 SEGMENTS_PATH = os.getenv("SEGMENTS_PATH", tempfile.mkdtemp())
@@ -32,22 +32,23 @@ def log() -> List[Tuple[str, str]]:
 
 
 @app.get("/kvs/{key}", responses={404: {"model": Message}})
-def get_key(key: str) -> Union[str, JSONResponse]:
+def get_key(key: str) -> JSONResponse:
     """Get the value stored at a particular key."""
     value = kvs.get(key)
     if value is None or value is TOMBSTONE:
-        return JSONResponse({"message": f"Key `{key}` was not found."}, 404)
-    return Response(status_code=200, content=value)
+        return JSONResponse(status_code=404, content={"message": f"Key `{key}` was not found."})
+    return JSONResponse(status_code=200, content=value)
 
 
 @app.put("/kvs", status_code=204)
-def set_key(key: str, value: str) -> None:
+def set_key(key: str, value: str) -> JSONResponse:
     """Set a key-value pair"""
     kvs.set(key, value)
-    return Response(status_code=204)
+    return JSONResponse(status_code=204)
 
 
 @app.delete("/kvs", status_code=204)
-def unset_key(key: str) -> None:
+def unset_key(key: str) -> JSONResponse:
     """Unset a key-value pair"""
     kvs.unset(key)
+    return JSONResponse(status_code=204)
